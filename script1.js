cf5130ac58c46d8c48fa135c0e3b2ed31d910921
Komal vanjari <78401491+komal145@users.noreply.github.com>
2022-02-18 17:13:25 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,41 @@
+var screen = document.querySelector("#screen");
+var btn = document.querySelectorAll(".btn");
+
+for (item of btn) {
+  item.addEventListener("click", (e) => {
+    btntext = e.target.innerText;
+
+    if (btntext == "x") {
+      btntext = "*";
+    }
+
+    if (btntext == "÷") {
+      btntext = "/";
+    }
+    screen.value += btntext;
+  });
+}
+
+function sin() {
+  screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+  screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+  screen.value = Math.tan(screen.value);
+}
+
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+  screen.value = Math.log(screen.value);
+}
+
+function pi() {
+  screen.value = 3.14159265359;
+}
